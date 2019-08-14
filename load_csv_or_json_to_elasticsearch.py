08756bbda0f817b285b9392277970cf19c9b32d9
Stanislav Příhoda <stanislav.prihoda@gmail.com>
2019-08-14 19:59:44 +0200

    script update to use bulk apis

--- load_csv_or_json_to_elasticsearch.py
+++ load_csv_or_json_to_elasticsearch.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-__all__ = ['ElasticDataLoader']
+__all__ = ['ElasticDataloader', 'ElasticDataloaderSet',
+           'ElasticDataloaderException']
 __version__ = '0.1'
 __author__ = 'Stanislav Prihoda'
 
@@ -12,116 +13,170 @@ import json
 import argparse
 import time
 import elasticsearch
+from elasticsearch.helpers import streaming_bulk
+import logging
+import locale
+
+LOG_LEVEL = logging.DEBUG
 
 
-class ElasticDataLoader:
+class ElasticDataloader(object):
     def __init__(self, **kwargs):
+        self._logger = logging.getLogger(__file__)
+        self.client = elasticsearch.Elasticsearch(**kwargs)
+
+    def load_dataset(self, es_dataset, chunk_size=500, **kwargs):
         try:
-            # any Elasticsearch py-client arguments can be passed
-            self.es = elasticsearch.Elasticsearch(**kwargs)
-        except (elasticsearch.ImproperlyConfigured, elasticsearch.ElasticsearchException) as e:
-            raise RuntimeError(e) from e
 
-    def load(self, input_file, es_index_name, **kwargs) -> dict:
-        """Higher level API to index a given file. If the type is not supported ValueError is raised.
+            if es_dataset.delete_index_first:
+                self._logger.debug(
+                    f"Attemting to delete the index {es_dataset.es_index_name} first as requested.")
+                self._delete_index(es_dataset.es_index_name)
+            self._logger.debug(
+                f"Parforming index {es_dataset.es_index_name} existence confirmation.")
+            self.client.indices.create(
+                index=es_dataset.es_index_name, ignore=400)
+            bulks_processed = 0
+            not_ok = []
+            for cnt, res in enumerate(streaming_bulk(self.client, self.csv_iterator(es_dataset), chunk_size)):
+                ok, result = res
+                if not ok:
+                    not_ok.append(result)
+                if cnt % chunk_size == 0:
+                    bulks_processed += 1
+                    self._logger.debug(
+                        f"Bulk number {bulks_processed} processed, already processed docs {cnt}.")
+                    if len(not_ok):
+                        self._logger.error(
+                            f"NOK DOCUMENTS (log limited to 10) in batch {bulks_processed}: {not_ok[-10:]}")
+                        not_ok = []
+            self._logger.info(
+                f"Refreshing index {es_dataset.es_index_name} to make indexed documents searchable.")
+            self.client.indices.refresh(index=es_dataset.es_index_name)
+        except (elasticsearch.ImproperlyConfigured) as e:
+            self._logger.error(
+                f"Issue with Elasticsearch client config {e}")
+            raise ElasticDataloaderException from e
+        except (elasticsearch.ElasticsearchException) as e:
+            self._logger.error(
+                f"There was an exception during indexing of data {e}")
+            raise ElasticDataloaderException from e
+        except (csv.Error, json.JSONDecodeError) as e:
+            self._logger.error(
+                f"Issue with the contents of the file: {es_dataset.input_file} {type(e)} {e}")
+            raise ElasticDataloaderException from e
+        except (IOError, ValueError) as e:
+            self._logger.error(
+                f"Issue with file: {es_dataset.input_file} {e}")
+            raise ElasticDataloaderException from e
+        else:
+            return cnt
 
-        Returns:
-            dict -- item example {'id of the doc':['result of indexing', 1 if indexed 0 if not]}
-        """
-        file_ext = input_file.split('.')[-1]
-        load_type = {'csv': self.load_csv,
-                     'json': self.load_json,
-                     'log': self.load_json}
-        # simple check on the extension - shall be improved in actual use
-        if file_ext not in load_type.keys():
-            raise ValueError(
-                'Given file format is not supported - only .csv or newline-delimited json (.json or .log).')
-        if kwargs.pop('delete_index_first', False):
-            self.delete_index(es_index_name)
-        return load_type[file_ext](input_file, es_index_name, **kwargs)
-
-    def load_csv(self, input_file, es_index_name, es_id_field="_id", es_index_start_from=1, **kwargs) -> dict:
-        operations = {}
-        with open(input_file) as csv_file:
+    def csv_iterator(self, es_dataset, **kwargs):
+        with open(es_dataset.input_file) as csv_file:
             csv_dict_reader = csv.DictReader(csv_file)
-            for cnt, row in enumerate(csv_dict_reader, start=es_index_start_from):
-                _id = row.get(es_id_field, cnt)
-                row.pop("_id", 0)
-                operations.update(self.index_single_document(
-                    row, es_index_name, _id))
-        return operations
+            for cnt, row in enumerate(csv_dict_reader):
+                yield self._prepare_document_for_bulk(es_dataset, row, cnt)
 
-    def load_json(self, input_file, es_index_name, es_id_field="_id", es_index_start_from=1, **kwargs) -> dict:
-        operations = {}
-        with open(input_file) as fp:
-            for cnt, line in enumerate(fp, start=es_index_start_from):
+    def ndjson_iterator(self, es_dataset, **kwargs):
+        with open(es_dataset.input_file) as fp:
+            for cnt, line in enumerate(fp):
                 row = json.loads(line)
-                _id = row.get(es_id_field, cnt)
-                row.pop("_id", 0)
-                operations.update(self.index_single_document(
-                    row, es_index_name, _id))
-        return operations
+                yield self._prepare_document_for_bulk(es_dataset, row, cnt)
 
-    def index_single_document(self, doc, es_index_name, _id) -> dict:
-        res = self.es.index(index=es_index_name,
-                            id=_id, body=doc)
-        result, doc_load_success = res['result'], 1 if (res['_shards']
-                                                        ['successful'] > 0) else 0
-        return {_id: [result, doc_load_success]}
+    def _prepare_document_for_bulk(self, es_dataset, row, cnt):
+        row["_id"] = row.get(es_dataset.es_id_field, cnt +
+                             es_dataset.es_index_start_from)
+        row["_index"] = es_dataset.es_index_name
+        row["_doc"] = "_doc"
+        return row
 
-    def delete_index(self, es_index_name):
+    def _delete_index(self, es_index_name):
         try:
-            res = self.es.indices.delete(index=es_index_name)
+            res = self.client.indices.delete(index=es_index_name)
         except elasticsearch.NotFoundError:
-            print("Index not found.")
+            self._logger.debug("Required index for delete not found.")
         else:
             return res
 
+    # ####### KEPT HERE JUST FOR EVENTUAL TESTING PURPOSES #######
+    # def load_csv_doc_by_doc(self, es_dataset, **kwargs) -> dict:
+    #     operations = {}
+    #     with open(es_dataset.input_file) as csv_file:
+    #         csv_dict_reader = csv.DictReader(csv_file)
+    #         for cnt, row in enumerate(csv_dict_reader):
+    #             _id = row.get(es_dataset.es_id_field, cnt +
+    #                           es_dataset.es_index_start_from)
+    #             row.pop("_id", 0)
+    #             res = self.client.index(index=es_dataset.es_index_name,
+    #                                     id=_id, body=row)
+    #             result, doc_load_success = res['result'], 1 if (res['_shards']
+    #                                                             ['successful'] > 0) else 0
+    #             operations[_id] = [result, doc_load_success]
+    #     return operations
+
+
+class ElasticDataloaderSet:
+    def __init__(self, input_file, es_index_name, es_id_field="_id", es_index_start_from=1, delete_index_first=False, **kwargs):
+        self.input_file, self.es_index_name, self.es_id_field, self.es_index_start_from, self.delete_index_first = input_file, es_index_name, es_id_field, es_index_start_from, delete_index_first
+        # kinda simplistic check on the extension - shall be improved in actual use
+        self.ext = self.input_file.split('.')[-1]
+        self.allowed_extensions = ['csv', 'json', 'log']
+        if self.ext not in self.allowed_extensions:
+            raise ValueError(
+                'Given file format is not supported - only .csv or newline-delimited json (.json or .log).')
+
+
+class ElasticDataloaderException(Exception):
+    pass
+
 
 def main(**kwargs):
+    logger = _get_logger(name=__file__)
     start = time.time()
+    logger.info("Script execution STARTED.")
     try:
         defined_params = {k: v for k, v in kwargs.items() if v is not None}
         es_host = defined_params.pop("es_host", "localhost:9200")
-        input_file = defined_params.get("input_file")
-        es_loader = ElasticDataLoader(hosts=[es_host])
-        results = es_loader.load(**defined_params)
-    except (RuntimeError) as e:
-        print(
-            f"Issue during setup \n{e}")
-        return 1
-    except (elasticsearch.ElasticsearchException) as e:
-        print(
-            f"There was an exception during indexing of data \n{e}")
-        return 1
-    except (csv.Error, json.JSONDecodeError) as e:
-        print(
-            f"Issue with the contents of the file: {input_file} \n{type(e)} {e}")
-        return 1
-    except (IOError, ValueError) as e:
-        print(
-            f"Issue with file: {input_file} \n{e}")
+        es_loader = ElasticDataloader(hosts=[es_host])
+        es_dataset = ElasticDataloaderSet(**defined_params)
+        docs_processed = es_loader.load_dataset(es_dataset)
+    except (ElasticDataloaderException) as e:
+        logger.error(
+            f"Script execution FAILED.", exc_info=True)
         return 1
     else:
         end = time.time()
-        print("========================================")
-        print(
-            f"Dataset from file {input_file} loaded to Elasticsearch at {es_host}")
-        print(f"Execution took {end - start} seconds.")
-        print(
-            f"Number of docs found: {len(results)}, loaded: {sum(n[1] for n in results.values())}.")
-        print(
-            f"Performed document operations: {set([value[0] for value in results.values()])}.")
-        print("========================================")
+        logger.info(
+            f"Script execution FINALIZED in {end - start} seconds. Dataset from file {es_dataset.input_file} loaded to Elasticsearch at {es_host}. Number of documents processed: {docs_processed}.")
         return 0
 
 
-def parse_args():
+def _get_logger(name=__name__, handler=logging.StreamHandler(sys.stdout)):
+    logger = logging.getLogger(name)
+    try:
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    except locale.Error as e:
+        logger.debug("Ignoring error %s when setting locale", e)
+    try:
+        logger.setLevel(LOG_LEVEL)
+    except NameError:
+        logger.setLevel(logging.INFO)
+    if not len(logger.handlers):
+        handler = handler
+        formater = logging.Formatter(
+            "%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+        handler.setFormatter(formater)
+        logger.addHandler(handler)
+    return logger
+
+
+def _parse_args():
     parser = argparse.ArgumentParser(
         description="Load data from CSV file or newline delimited JSON file to a running Elasticsearch cluster.",
         epilog=f"Example: python3 {__file__} my-data.csv my-elastic-index")
     parser.add_argument(
-        'input_file', help='Path to the input csv/json file. Must end with .csv or .json/.log')
+        'input_file', help='Path to the input csv/json file. Extension check on .csv or .json/.log')
     parser.add_argument(
         'es_index_name', help='Name of the index in which to index the documents.')
     parser.add_argument('--es_host',
@@ -137,4 +192,4 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    sys.exit(main(**parse_args()))
+    sys.exit(main(**_parse_args()))
